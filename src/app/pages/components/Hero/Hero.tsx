c79681e5fda8c0f83fffc3f86ab3738bbbd7006d
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-12-20 20:16:28 -0300

    correção paliativa

--- src/app/pages/components/Hero/Hero.tsx
+++ src/app/pages/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+'use client'
+import { Link } from 'react-scroll';
 import './Hero.css';
 
 const Hero = () => {
@@ -7,7 +8,7 @@ const Hero = () => {
       <div className="header-text">
         <h1>Contrato Agora</h1>
         <p>Sua solução ideal para contratos personalizados</p>
-        <Link id="CONTRATO" className="btn" href={''}>
+        <Link id="CONTRATO" className="btn" to='CONTRATOS' >
           Criar Contrato
         </Link>
       </div>
--- src/app/pages/components/footer/Footer.tsx
+++ src/app/pages/components/footer/Footer.tsx
@@ -3,11 +3,11 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='rodape'>
-        <p>Desenvolvido por QuedSoft.Inc. Todos os direitos reservados</p>
-        <ul>
-            <li>Termos de Servicos</li>
-            <li>Politica de Privacidade</li>
-        </ul>
+      <p>Desenvolvido por <a href='https://quedsoft.vercel.app/' target='_blank'>QuedSoft.Inc.</a> Todos os direitos reservados</p>
+      <ul>
+        <li>Termos de Servicos</li>
+        <li>Politica de Privacidade</li>
+      </ul>
     </div>
   )
 }
