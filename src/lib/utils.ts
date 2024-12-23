94370ef870fd8ea455e50ba0bd639d96272c1443
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-12-23 16:37:21 -0300

    corrigindo erro do split

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
 export function formatarData(Data: string): string {
-  const [year, month, day] = Data.split('-');
+  if (typeof Data !== 'string' || Data.trim() === '') {
+    console.error('Data inválida ou vazia');
+    return 'Data inválida';
+  }
+
+  // Verificando se o formato de data é no padrão 'YYYY-MM-DD'
+  if (Data.length !== 10 || Data[4] !== '-' || Data[7] !== '-') {
+    console.error('Formato de data inválido');
+    return 'Data inválida';
+  }
+
+  // Extraindo as partes diretamente
+  const year = Data.substring(0, 4);
+  const month = Data.substring(5, 7);
+  const day = Data.substring(8, 10);
+
   return `${day}/${month}/${year}`;
-};
+}
