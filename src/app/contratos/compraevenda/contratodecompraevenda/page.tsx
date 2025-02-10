ce331e612e11104881862a5ec96e485bd26cbb9e
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2025-02-10 13:29:31 -0300

    ainda corrigindo

--- src/app/contratos/compraevenda/contratodecompraevenda/page.tsx
+++ src/app/contratos/compraevenda/contratodecompraevenda/page.tsx
@@ -1,19 +1,19 @@
-'use client'
 import Navbar from "@/app/pages/components/Navbar-outside/Navbar";
 import Footer from "@/app/pages/components/footer/Footer";
 import Link from "next/link";
 import './css/index.css';
 
-const ContratoCV = () => {
+
+export default function FormularioCompraVenda() {
   return (
     <>
-
       <Navbar />
+      <div className="hospedagem-cv">
 
-      <div className="container-cv">
         <div className="title">
           <h1>Modelo de Contrato de Compra e Venda</h1>
         </div>
+
         <div className="text">
           <p>
             O contrato de compra e venda é o documento utilizado para que uma pessoa possa vender um bem de forma
@@ -31,7 +31,9 @@ const ContratoCV = () => {
             Nosso modelo de contrato de compra e venda é revisado mensalmente, conforme o Código Civil (Lei Federal nº 10.406).
 
             Ao utilizar nosso contrato, você pode ter certeza de que estará realizando uma negociação transparente e em conformidade com a legislação brasileira.
+
           </p>
+
         </div>
 
         <div className="texto-2">
@@ -56,32 +58,15 @@ const ContratoCV = () => {
             transação.
           </p>
         </div>
-        <div className="texto-3">
-          <h1 className="subtitle">DIFERENÇAS ENTRE O CONTRATO DE COMPRA E VENDA E A SUA PROMESSA</h1>
-          <p>
-            A negociação pode ser realizada por meio de um contrato simples de compra e venda, também conhecido como compromisso de compra e venda, ou por meio de uma promessa de compra e venda.
-
-            No compromisso de compra e venda, o contrato é irrevogável, ou seja, as partes não têm a possibilidade de arrependimento.
-            Isso significa que, se uma das partes desistir da negociação, o contrato pode ser utilizado judicialmente para exigir o cumprimento dos termos
-            acordados.
-
-            Por outro lado, a promessa de compra e venda é considerada um contrato preliminar, que permite o arrependimento por uma das partes.
-            Se o contrato permite a desistência do negócio, mesmo com a aplicação de uma multa (que é opcional), ele será classificado como uma promessa de
-            compra e venda.
-          </p>
-        </div>
 
         <div className="preencherContrato">
           <button className="btn">
-            <Link className='btn' href="contratodecompraevenda/formulario">Preencher Contrato</Link>
+            <Link className='btn' href="locacaovagadegaragem/formulario">Preencher Contrato</Link>
           </button>
         </div>
-
       </div>
 
       <Footer />
     </>
-  )
+  );
 }
-
-export default ContratoCV;
